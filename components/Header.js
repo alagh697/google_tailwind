5f98c1e71e165d6694026e4652ae780db3552deb
alagh697 <37829292+alagh697@users.noreply.github.com>
2021-09-19 13:10:29 +0200

    app build

--- components/Header.js
+++ components/Header.js
@@ -37,7 +37,7 @@ function Header() {
                 <form className='flex px-6 py-3 ml-10 mr-5 border border-gray-200 
                 rounded-full shadow-lg max-w-3xl items-center flex-grow'>
                     <input ref={searchInputRef} type="text" className="flex-grow w-full 
-                    focus:outline-none"/>
+                    focus:outline-none" defaultValue={router.query.term}/>
 
                     <XIcon className="h-7 sm:mr-3 text-gray-700 cursor-pointer transition duration-100 
                     transform hover:scale-125"
